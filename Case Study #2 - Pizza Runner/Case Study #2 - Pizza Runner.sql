e6e721a4b197b740879864d693f0a636507a604c
kaushalJ2904 <joshikaushal1105@gmail.com>
2024-01-01 11:33:54 +0530

    changes to week#2 project

--- Case Study #2 - Pizza Runner/Case Study #2 - Pizza Runner.sql
+++ Case Study #2 - Pizza Runner/Case Study #2 - Pizza Runner.sql
@@ -59,3 +59,160 @@
 1 - If Danny wants to expand his range of pizzas - how would this impact the existing data design? Write an INSERT statement to demonstrate what would happen if a new Supreme pizza with all the toppings was added to the Pizza Runner menu?
 
 */
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--A. Pizza Metrics
+
+--Question_1 - How many pizzas were ordered?
+
+SELECT 
+	COUNT(*) as total_pizzas
+FROM pizza_runner.customer_orders
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+--Question_2 - How many unique customer orders were made?
+
+SELECT 
+	COUNT(DISTINCT(order_id)) as unique_customer_order
+FROM pizza_runner.customer_orders
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+--Question_3 - How many successful orders were delivered by each runner?
+
+SELECT * FROM pizza_runner.customer_orders
+
+SELECT 
+	runner_id, 
+	COUNT(1) as successful_orders  
+FROM pizza_runner.runner_orders
+WHERE cancellation is NULL
+GROUP BY runner_id
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+--Question_4 - How many of each type of pizza was delivered?
+
+SELECT 
+	co.pizza_id,
+	pn.pizza_name,
+	COUNT(co.pizza_id) as each_type_of_pizza
+FROM pizza_runner.customer_orders co
+LEFT JOIN pizza_runner.pizza_names pn
+ON co.pizza_id = pn.pizza_id
+LEFT JOIN pizza_runner.runner_orders ro
+ON co.order_id = ro.order_id
+WHERE cancellation is null
+GROUP BY co.pizza_id, pn.pizza_name
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+--Question_5 - How many Vegetarian and Meatlovers were ordered by each customer?
+
+SELECT 
+		c.customer_id, 
+		p.pizza_name, 
+		COUNT(1) as count_of_pizza
+FROM pizza_runner.customer_orders c
+LEFT JOIN pizza_runner.pizza_names p
+ON c.pizza_id = p.pizza_id
+GROUP BY c.customer_id, p.pizza_name
+ORDER BY c.customer_id
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+--Question_6 - What was the maximum number of pizzas delivered in a single order?
+
+
+SELECT 
+	TOP 1 c.customer_id,c.order_id, COUNT(1) as max_pizza
+FROM pizza_runner.customer_orders c
+LEFT JOIN pizza_runner.runner_orders r
+ON c.order_id = r.order_id
+WHERE cancellation is null
+GROUP BY c.customer_id,c.order_id
+ORDER BY COUNT(1) DESC
+
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+--Question_7 - For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+
+SELECT  c.customer_id,
+		SUM(CASE WHEN c.exclusions is not null OR c.extras is not null THEN 1 ELSE 0 END) as pizza_with_change,
+		SUM(CASE WHEN c.exclusions is null AND c.extras is null THEN 1 ELSE 0 END) as pizza_with_no_change
+FROM pizza_runner.customer_orders c
+LEFT JOIN pizza_runner.runner_orders r
+ON c.order_id = r.order_id
+WHERE r.cancellation is NULL
+GROUP BY c.customer_id
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+--Question_8 - How many pizzas were delivered that had both exclusions and extras?
+
+SELECT 
+		c.customer_id,
+	    SUM(CASE WHEN c.exclusions is not null and c.extras is not null THEN 1 ELSE 0 END) both_changes_in_pizza
+FROM pizza_runner.customer_orders c
+LEFT JOIN pizza_runner.runner_orders r
+ON c.order_id = r.order_id
+WHERE r.cancellation is NULL
+GROUP BY c.customer_id
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+--Question_9 - What was the total volume of pizzas ordered for each hour of the day?
+
+SELECT 
+		DATEPART(HOUR, order_time) as hr,
+		COUNT(1) no_of_pizzas_ordered
+FROM pizza_runner.customer_orders
+GROUP BY DATEPART(HOUR, order_time)
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+--Question_10 - What was the volume of orders for each day of the week?
+
+
+SELECT
+	DATEPART(WEEKDAY, order_time) as week_day,
+    DATENAME(WEEKDAY, order_time) AS week_day_name,
+    COUNT(1) AS no_of_pizzas_ordered
+FROM 
+    pizza_runner.customer_orders
+GROUP BY 
+	DATEPART(WEEKDAY, order_time),
+    DATENAME(WEEKDAY, order_time);
+
+   
+
+
+
+
+
+
+
+
+
